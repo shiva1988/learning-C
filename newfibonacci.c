64f247f310b5d71237bd492e1ef2f57ace67ff22
Shiva Barzili <lotfa100@mail.chapman.edu>
2015-11-17 15:06:25 -0800

    it generates positive numbers

--- newfibonacci.c
+++ newfibonacci.c
@@ -1,17 +1,26 @@
+/*From the previous homework, we have noticed that our fibonacci had produced so me negative numbers.
+ *Such an unexpected result due to the limited range of an int.
+ *This is called overflow or rollover.Once you go past the maximum number that c an be represented ,it rolls over and starts from the begining .
+ *In this project , we aim at finding a better data type that will avoid us a 
+ weird situation as seen with integer. for the best data type will be long 
+ double.*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void fib(long double);
+void fib(long double); //declare the prototype of the fib function.
 int main()
 {
-	long double number;
-	printf(" Please my friend ismael give me an integer :");
+	long double number;/*declare an integer variable that will hold the numbers that will be punch in by the user.*/
+	printf(" Please my friend ismael give me an integer :");/*prompt the user to enter a number.*/
 	scanf("%Lf",&number);
 
 	fib(number);
 return 0;
 }
-
+/*I explained all of things in the previous code,just we change here int to 
+  longe doble*/
 void fib(long double n)
 {
 	long double i,f1,f2,fibo=0;
