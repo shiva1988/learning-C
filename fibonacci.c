65694430acbc8115af727609535eeb821571140f
Shiva Barzili <lotfa100@mail.chapman.edu>
2015-11-17 15:04:59 -0800

    this is with comment.

--- fibonacci.c
+++ fibonacci.c
@@ -5,25 +5,25 @@ void fib(int);
 int main()
 {
 	int number;
-	printf(" Please my friend ismael give me an integer :");
+	printf(" Please my friend ismael give me an integer :");/* I ask to user give me number*/
 	scanf("%d",&number);
 
 	fib(number);
 return 0;
 }
 
-void fib(int n)
+void fib(int n)//I define a function to generate fibonacci numbers.
 {
 	int i,f1,f2,fibo=0;
-	f1=1;
+	f1=1;// we put f1=f2=1 based on fibonacci numbers.
 	f2=1;
 	if(n>=2)
 	{
 		printf("%d\n",f1);
 		printf("%d\n",f2);
-		//printf("\n");
 		for(i=3;i<=n;i++)
-		{
+		{/*by using loop,we creat new number 
+			that is sum of the previous two*/
 			fibo=f1+f2;
 			f1=f2;
 			f2=fibo;
